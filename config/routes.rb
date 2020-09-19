96506704e9e1a64b0f6ebe37c4611e64c4035503
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-18 23:41:22 -0300

    ADD: text and title to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  "pages/aboutus"
+  get  "pages/about_us"
   get "pages/test"
   get  "users/login"
 
