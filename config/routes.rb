8e0d7011dcf9616beaf7d03e1ce844cb79ac69f8
Javier <javier.arancibia.reyes@hotmail.com>
2020-09-18 23:33:38 -0300

    ADD: links to pages, and created login and about us paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  "users/login"
-  get  "pages/about_us"
+  get  "pages/aboutus"
   get "pages/test"
+  get  "users/login"
+
   root "pages#index"
 end
